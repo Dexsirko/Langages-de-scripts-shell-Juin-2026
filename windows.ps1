4d9678f00d49da3e37489aaefe0252fcecd8673d
dexsirko <dexsirko@gmail.com>
2026-05-05 18:18:41 +0200

    Simplify installation checks by removing output
    Removed output messages for installed packages.

--- windows.ps1
+++ windows.ps1
@@ -2,27 +2,15 @@
 if (!(choco list | Select-String "7zip.install")){
     choco install 7zip.install -y
 }
-else
-{
-    Write-Output "7Zip Installed"
-}
 
 # Check installation of Python3
 if (!(choco list | Select-String "python")){
     choco install python -y
 }
-else
-{
-    Write-Output "Python3 Installed"
-}
 
 # Check installation of Notepad++
 if (!(choco list | Select-String "notepadplusplus.install")){
     choco install notepadplusplus.install -y
-}
-else
-{
-    Write-Output "NotePad++ Installed"
 }
 
 # AutoUpdate of all packages
